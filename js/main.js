7814bc249535b744538867353e88658a16b46555
hudsonk2 <hudsonk2@seattleu.edu>
2016-11-06 15:51:53 -0500

    updated css and js files

--- js/main.js
+++ js/main.js
@@ -12,10 +12,7 @@ $(document).on('ready', function() {
     return false; // prevent a reload of the page which causes null in search box 
   });
 
-
-
   function searchImages(tags) {
-
     // create variable to shorten long API string 
     var flickerAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
 
@@ -36,20 +33,23 @@ $(document).on('ready', function() {
           // setting list item to new list 
 
           $('<img>').attr("src", item.media.m).attr("title", item.title).attr("alt", item.description).appendTo(newList);
+
           $('<p></p>').text("Title : " + item.title).appendTo(newList);
-          $('<p></p>').text("Date : " + item.date_taken).appendTo(newList);
+          $('<p></p>').text("Date/Time : " + item.date_taken).appendTo(newList);
           $('<p></p>').text("Author : " + item.author).appendTo(newList);
           $('<a></a>').attr('href', item.link).text('View on Flickr.').appendTo(newList);
+
 
           // then appent the list to the image here
           newList.appendTo("#images");
           // after 10 pictures discontinue list 
-          if (i === 11) { 
+          if (i === 11) {
             return false;
           }
         });
       });
   }
-
+  // I changed the footer text with Jquery.
+  $("footer").text("Kevin Hudson, WATS3020, Fall 2016.");
 
 });
